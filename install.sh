0261660017a8bb13733c742e3fe06b53f038ba37
Mitchell  Douglass <mrdouglass95@gmail.com>
2023-06-07 03:28:38 +0000

    copy home_files

--- install.sh
+++ install.sh
@@ -7,4 +7,20 @@ git config --global alias.can 'commit --ammend --no-edit'
 git config --global alias.cm 'commit -m'
 git config --global alias.lg 'log --graph --all --oneline --abbrev-commit'
 
-cp ~/.dotfiles/.bash_aliases ~/
+current_dir="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+dotfiles_source="${current_dir}/home_files"
+
+while read -r file; do
+
+    relative_file_path="${file#"${dotfiles_source}"/}"
+    target_file="${HOME}/${relative_file_path}"
+    target_dir="${target_file%/*}"
+
+    if test ! -d "${target_dir}"; then
+        mkdir -p "${target_dir}"
+    fi
+
+    printf 'Installing dotfiles symlink %s\n' "${target_file}"
+    ln -sf "${file}" "${target_file}"
+
+done < <(find "${dotfiles_source}" -type f)
