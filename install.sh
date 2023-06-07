5e3a605d93795638c9f19529c63d0c9e6ef26d29
Mitchell  Douglass <mrdouglass95@gmail.com>
2023-06-07 02:54:13 +0000

    add install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+# git aliases
+git config --global alias.co checkout
+git config --global alias.ca 'commit --amend'
+git config --global alias.can 'commit --ammend --no-edit'
+git config --global alias.cm 'commit -m'
+git config --global alias.lg 'log --graph --all --oneline --abbrev-commit'
