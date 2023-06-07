6cbd1359bcaa8a09ee007274029d5fbd0db978aa
Mitchell  Douglass <mrdouglass95@gmail.com>
2023-06-07 03:08:31 +0000

    copy git aliases to home directory

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ git config --global alias.ca 'commit --amend'
 git config --global alias.can 'commit --ammend --no-edit'
 git config --global alias.cm 'commit -m'
 git config --global alias.lg 'log --graph --all --oneline --abbrev-commit'
+
+cp .bash_aliases ~/
