654510a437cfd6b290fdb0b6f64003b33d8a3cdc
Mitchell  Douglass <mrdouglass95@gmail.com>
2023-06-07 20:35:58 -0400

    Fig git alias

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # git aliases
 git config --global alias.co checkout
 git config --global alias.ca 'commit --amend'
-git config --global alias.can 'commit --ammend --no-edit'
+git config --global alias.can 'commit --amend --no-edit'
 git config --global alias.cm 'commit -m'
 git config --global alias.lg 'log --graph --all --oneline --abbrev-commit'
 
